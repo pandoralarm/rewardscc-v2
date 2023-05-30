f6c13a1d3486a54e6be9fb62b109309123da6f21
Alan Go <55218308+pandoralarm@users.noreply.github.com>
2023-05-30 16:31:01 +0700

    revision

--- src/pages/showcase-infinum.js
+++ src/pages/showcase-infinum.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import Layout from '@theme/Layout';
-import ShowcaseGrid from '../theme/sections/showcase-grid/showcase-grid';
-import Footer from '../theme/sections/footer/footer';
-import Foot from '../theme/sections/foot/foot';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Layout from "@theme/Layout";
+import ShowcaseGrid from "../theme/sections/showcase-grid/showcase-grid";
+import Footer from "../theme/sections/footer/footer";
+import Foot from "../theme/sections/foot/foot";
 
 export default function Showcase() {
-	const context = useDocusaurusContext();
-	const {siteConfig = {}} = context;
+    const context = useDocusaurusContext();
+    const { siteConfig = {} } = context;
 
-	return (
-		<Layout
-			title={'Showcase Infinum'}
-			description={siteConfig.tagline}
-			keywords={siteConfig.customFields.keywords}
-			metaImage={useBaseUrl(`img/${siteConfig.customFields.image}`)}
-			wrapperClassName={'page-showcase'}
-		>
-			<ShowcaseGrid privateType={true}/>
-			<Footer />
-			<Foot />
-		</Layout>
-	);
-};
+    return (
+        <Layout title={"Showcase Infinum"} description={siteConfig.tagline} keywords={siteConfig.customFields.keywords} metaImage={useBaseUrl(`img/${siteConfig.customFields.image}`)} wrapperClassName={"page-showcase"}>
+            <ShowcaseGrid privateType={true} />
+            <Footer />
+        </Layout>
+    );
+}
